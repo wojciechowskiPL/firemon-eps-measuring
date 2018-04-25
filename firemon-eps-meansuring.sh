1c85f2cbdcb88ec4fea5651139299a2be7ba9f89
devu <devurien@gmail.com>
2018-04-25 09:18:12 +0200

    Add OPSEC-LEA to tshark Add color Change variable to uppercase

--- firemon-eps-meansuring.sh
+++ firemon-eps-meansuring.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
- 
-output_file=/tmp/eps.txt
-interface=ens192
- 
-#Tshark
-tshark="tshark -a duration:10 -i $interface udp port 514"
- 
-#Tshark log to file
-$tshark > $output_file
- 
-#Counting line
-cat $output_file | wc -l
- 
-#Remove/delete the outputfile`
-rm $output_file 2>/dev/null
+
+OUTPUT_FILE=/tmp/eps.txt
+INTERFACE=ens192
+DURATION=10
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "[+] This process take ${DURATION} sec"
+
+# Tsharh
+# udp port 514   - Syslog
+# tcp port 18184 - OPSEC LEA
+TSHARK="tshark -a duration:${DURATION} -i ${INTERFACE} tcp port 6514 or udp port 514 udp port 18184"
+
+
+#Count line
+echo -e "[+] Detected: $($TSHARK > $OUTPUT_FILE)  ${RED}$(cat $OUTPUT_FILE | wc -l) ${NC}EPS"
+
+#Remove/delete the outputfile
+rm $OUTPUT_FILE 2>/dev/null
